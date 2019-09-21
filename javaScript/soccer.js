96e89637543e195ca7c422d8032ed642debf9869
Umair Mustafa <umairhassaaan@gmail.com>
2019-09-21 11:03:12 +0500

    latest

--- javaScript/soccer.js
+++ javaScript/soccer.js
@@ -4,22 +4,20 @@ const context = canvas.getContext( "2d" );
 
 
 //declaring and intializing variables.
-let ball = { 
-                x: Math.floor( window.innerWidth  / 2 ),
+let ball = {    x: Math.floor( window.innerWidth  / 2 ),
                 y: Math.floor( window.innerHeight / 2 ), 
                 velocity: { x: 0, y: 0 },
                 mass: 1,
                 radius:  Math.floor( window.innerHeight / 12 ) / 2  
-            };
+};
 
 
-let player = { 
-                x: 0,
+let player = {  x: 0,
                 y: 0,
                 mass: 1,
-                velocity: { x: 2, y: 3 },
+                velocity: { x: 0.5, y: 0 },
                 radius: Math.floor( window.innerHeight / 10 ) / 2 
-            };
+};
 
 
 
@@ -32,8 +30,8 @@ setInterval( movingBall, 10 );
 
 //random velocity for x and y axis.
 function randomVelocity(){
-        ball.velocity.x = ( Math.random() - 0.5 ) * 5;
-        ball.velocity.y  = ( Math.random() - 0.5 ) * 5;
+        ball.velocity.x = ( Math.random() - 0.5 ) * 10;
+        ball.velocity.y  = ( Math.random() - 0.5 ) * 10;
 }
 
 
@@ -66,15 +64,17 @@ function groundDesign(){
     context.beginPath();
     context.arc( canvas.width / 2, canvas.height / 2, canvas.width / 10 + canvas.height / 10, 0 , 2 * Math.PI  );
     context.stroke();
-}
+}   
 
 
 
-canvas.onmousemove = function ( evnet ){
-    //condition to stick player in its boundry.
-    if( evnet.clientX < ( canvas.width / 2 ) - player.radius){
-        player.x = evnet.clientX, player.y = evnet.clientY; 
-    }
+onmousemove = function ( event ){
+    const tempPlayer = { x: event.clientX, y: event.clientY  }; //temppLyaer to check player's ball and moving ball is overlaping or not;
+    const td = distance( ball, tempPlayer );
+    
+    if( tempPlayer.x + player.radius < canvas.width / 2 )
+        if( td  > ball.radius + player.radius)
+            player.x = event.clientX , player.y = event.clientY;
     user();
 }
 
@@ -97,21 +97,22 @@ function movingBall(){
     user();
     ball.radius =  Math.floor( (canvas.height / 12) / 2 );
 
-
     //changing state of flag when x and y axis of ball touches the boundry.
-   
-
     if( ball.x - ball.radius <= 0 || ball.x + ball.radius >= canvas.width )
         ball.velocity.x = -ball.velocity.x;
     else if( ball.y - ball.radius <= 0 || ball.y + ball.radius >= canvas.height )
         ball.velocity.y = -ball.velocity.y;
 
 
-    const td = distance( ball, player )
+
+    const td = distance( ball, player );
     if ( td <= ball.radius + player.radius ){
-        resolveCollision( ball, player );
-        // console.log( ball.radius + player.radius, td );
+        // console.log( td, ball.radius + player.radius );
+            let rotatedVelocities =  rotate(); 
+            ball.velocity.x = -rotatedVelocities.x;
+            ball.velocity.y = -rotatedVelocities.y; 
     }
+    
 
     ball.x += ball.velocity.x;
     ball.y += ball.velocity.y;
@@ -136,46 +137,13 @@ function distance(ball, player){
 
 
 
-function rotate(velocity, angle) {
+function rotate() {
+    const angle = -Math.atan2(ball.y,ball.x);
+
     const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+        x: ball.velocity.x * Math.cos(angle) -  ball.velocity.y * Math.sin(angle),
+        y: ball.velocity.x * Math.sin(angle) +  ball.velocity.y * Math.cos(angle)
     };
-
+    
     return rotatedVelocities;
-}
-
-
-
-function resolveCollision(ball, player) {
-    const xVelocityDiff = ball.velocity.x - player.velocity.x;
-    const yVelocityDiff = ball.velocity.y - player.velocity.y;
-
-    const xDist = player.x - ball.x;
-    const yDist = player.y - ball.y;
-
-    // Prevent accidental overlap of balls
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-
-        // Grab angle between the two colliding balls
-        const angle = -Math.atan2(player.y - ball.y, player.x - ball.x);
-
-        // Store mass in var for better readability in collision equation
-        const m1 = player.mass;
-        const m2 = ball.mass;
-
-        // Velocity before equation
-        const u1 = rotate(ball.velocity, angle);
-        const u2 = rotate(player.velocity, angle);
-
-        // Velocity after 1d collision equation
-        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
-
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = rotate(v1, -angle);
-
-        // Swap ball velocities for realistic bounce effect
-        ball.velocity.x = vFinal1.x;
-        ball.velocity.y = vFinal1.y;
-    }
 }
